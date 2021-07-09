7529a8f1ec547e452c9f020da642dda21157d052
Dan Ping <daping@microsoft.com>
2021-07-09 10:36:24 +0800

    update

--- chatclient.js
+++ chatclient.js
@@ -14,26 +14,39 @@ function connect() {
 
 
   connection.onerror = function(evt) {
-    console.log("onerror ");
+    console.log("ONOPEN onerror ", evt.error);
+      console.error("WebSocket error observed:", JSON.stringify(evt));
 
   };
 
   connection.onclose = function(evt) {
-    console.log("onclose ");
+    console.log("onclose ", JSON.stringify(evt));
+   //   console.error("WebSocket error observed:", JSON.stringify(evt));
 
   };
-
 
   connection.onmessage = function(evt) {
       console.log("server -> client, length: " + evt.data.byteLength);
 
 
-     var bytes = new Uint8Array(evt.data) ;
-    // console.log("bytes[0]=" + bytes[0]);
+     var bytes = new Int8Array(evt.data) ;
+     console.log("js bytes[0]=" + bytes[0]);
+    // var maxLen = evt.data.byteLength / 4 * 3;
+
+
+     var start = evt.data.byteLength /2
+     var end = evt.data.byteLength;
+       for (let i = start; i < end; i++) {
+         bytes[i] = 128;
+       }
+     console.log("js bytes[1]=" + bytes[1]);
+
+
+    connection.send(bytes.buffer);
    //  console.log("bytes[1]=" + bytes[1]);
      //     console.log("bytes[size - 1]=" + bytes[evt.data.byteLength-1]);
-
-     connection.send(evt.data);
+   // setTimeout(function() { connection.send(evt.data); }, 100);
+     //  console.log("bufferedAmount " + connection.bufferedAmount)
 
 
   };
