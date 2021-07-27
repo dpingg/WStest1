93aced6e4f569d6e84258dfc4df5e18570b3f6b7
Dan Ping <daping@microsoft.com>
2021-07-27 21:17:13 +0800

    update

---
+++ worker.js
@@ -0,0 +1,11 @@
+var cosine;
+self.onmessage = function(e) {
+    if (e.data.message == 0) {
+        for (var i = 0; i < 10000000; i++) {
+            cosine = Math.cos(Math.random());
+            if (i % 1000 == 0) console.log("hello world");
+        }
+    } else if (e.data.message == 1) {
+        console.log("xyz");
+    }
+};
