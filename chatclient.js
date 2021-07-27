6658d45eec14be4f6f696f3143b326789c5c5213
Dan Ping <daping@microsoft.com>
2021-07-27 21:21:50 +0800

    update

--- chatclient.js
+++ chatclient.js
@@ -3,7 +3,7 @@ worker.onmessage = function(e) {
     if (e.data.message == 0) {
         for (var i = 0; i < 10000000; i++) {
             cosine = Math.cos(Math.random());
-            if (i % 1000 == 0) console.log("hello world");
+            if (i % 1000000 == 0) console.log("hello world " + i);
         }
     } else if (e.data.message == 1) {
         console.log("xyz");
